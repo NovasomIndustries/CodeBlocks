2618a41fd5bba31808d4459682d58c66add81daa
NovasomIndustries <filippo.visocchi@novasomindustries.com>
2017-02-27 15:14:26 +0100

    Changed some sdl2 apps

--- Apps/SdlExample/main.c
+++ Apps/SdlExample/main.c
@@ -2,15 +2,17 @@
 #include <SDL.h>
 #include <SDL_image.h>
 
-#define WIDTH 1280
-#define HEIGHT 800
-#define IMG_PATH1 "exit1.png"
-#define IMG_PATH2 "exit2.png"
-#define IMG_PATH3 "exit3.png"
-#define IMG_PATH4 "exit4.png"
-#define IMG_PATH5 "exit5.png"
-#define IMG_PATH6 "exit6.png"
+#define WIDTH 800
+#define HEIGHT 480
+#define IMG_PATH0 "ks00.png"
+#define IMG_PATH1 "ks01.png"
+#define IMG_PATH2 "ks02.png"
+#define IMG_PATH3 "ks03.png"
+#define IMG_PATH4 "ks04.png"
+#define IMG_PATH5 "ks05.png"
+#define IMG_PATH6 "ks06.png"
 
+SDL_Texture *img0 = NULL;
 SDL_Texture *img1 = NULL;
 SDL_Texture *img2 = NULL;
 SDL_Texture *img3 = NULL;
@@ -18,18 +20,44 @@ SDL_Texture *img4 = NULL;
 SDL_Texture *img5 = NULL;
 SDL_Texture *img6 = NULL;
 
+//#define SINGLE_IMAGE
+
 int main (int argc, char *argv[])
 {
 SDL_Window *win = NULL;
 SDL_Renderer *renderer = NULL;
 int img_ptr = 0 , events = 0;
+FILE *fp;
+char    video_string[32];
+int  width,height;
 
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-			return 1;
+        return 1;
+    fp = fopen("/sys/class/graphics/fb0/mode","r");
+    if ( !fp )
+        return -1;
+    fread(video_string,1,32,fp);
+    fclose(fp);
+    if (strstr(video_string,"800x480"))
+    {
+        width = 800;
+        height = 480;
+    }
+    if (strstr(video_string,"800x600"))
+    {
+        width = 800;
+        height = 600;
+    }
+    if (strstr(video_string,"1280x800"))
+    {
+        width = 1280;
+        height = 800;
+    }
 
-	win = SDL_CreateWindow("Image Loading", 0, 0, WIDTH, HEIGHT, 0);
+	win = SDL_CreateWindow("Image Loading", 0, 0, width, height, 0);
 	renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
 
+	img0 = IMG_LoadTexture(renderer, IMG_PATH0);
 	img1 = IMG_LoadTexture(renderer, IMG_PATH1);
 	img2 = IMG_LoadTexture(renderer, IMG_PATH2);
 	img3 = IMG_LoadTexture(renderer, IMG_PATH3);
@@ -38,11 +66,18 @@ int img_ptr = 0 , events = 0;
 	img6 = IMG_LoadTexture(renderer, IMG_PATH6);
 
     SDL_RenderClear(renderer);
-	SDL_RenderCopy(renderer, img5, NULL, NULL);
+    #ifdef  SINGLE_IMAGE
+	SDL_RenderCopy(renderer, img1, NULL, NULL);
+    #else
+	SDL_RenderCopy(renderer, img1, NULL, NULL);
+    #endif // SINGLE_IMAGE
     SDL_RenderPresent(renderer);
+
 	while (1)
 	{
-	/*
+        #ifdef  SINGLE_IMAGE
+            sleep(1);
+        #else
 		SDL_Event e;
         events = 0;
 		if ( SDL_PollEvent(&e) )
@@ -57,26 +92,28 @@ int img_ptr = 0 , events = 0;
                 printf("Event %d\n",e.type);
             }
 		}
-        SDL_Delay( 1000 );
-        events++;
-        SDL_RenderCopy(renderer, img6, NULL, NULL);
 
         if ( events != 0 )
         {
             SDL_RenderClear(renderer);
             switch ( img_ptr )
             {
-                case 0 : SDL_RenderCopy(renderer, img5, NULL, NULL); break;
-                case 1 : SDL_RenderCopy(renderer, img5, NULL, NULL); break;
-                case 2 : SDL_RenderCopy(renderer, img5, NULL, NULL); break;
-                case 3 : SDL_RenderCopy(renderer, img5, NULL, NULL); break;
+                case 0 : SDL_RenderCopy(renderer, img1, NULL, NULL); break;
+                case 1 : SDL_RenderCopy(renderer, img2, NULL, NULL); break;
+                case 2 : SDL_RenderCopy(renderer, img3, NULL, NULL); break;
+                case 3 : SDL_RenderCopy(renderer, img4, NULL, NULL); break;
+                case 4 : SDL_RenderCopy(renderer, img5, NULL, NULL); break;
+                case 5 : SDL_RenderCopy(renderer, img6, NULL, NULL); break;
             }
             SDL_RenderPresent(renderer);
             img_ptr++;
-            img_ptr &= 3;
+            if ( img_ptr > 5 )
+                img_ptr = 0;
             SDL_Delay( 10 );
             events = 0;
         }
+        #endif
+	/*
         */
 	}
 	SDL_DestroyTexture(img1);
